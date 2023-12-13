f3c13de32d0646596fe3bf09e240b13cd03d132e
Hakim Nazri <87183777+hakimnazry24@users.noreply.github.com>
2023-12-13 05:56:21 +0000

    change nav bar for desktop

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -12,9 +12,19 @@ const NavBar = () => {
   return (
     <>
         <nav className="font-itim w-full fixed top-0 bg-slate-200 z-10">
-          <div className="p-3 px-5 flex justify-between items-center">
-            <a href="/"><span className="text-xl lg:text-2xl">CariMentor</span></a>
-            <button id="mobile-navbar-btn" onClick={toggleMobileNavBar} className="bg-darkgreen p-3 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></button>
+          <div className="md:flex items-center justify-between p-3 px-5">
+            <div className="p-3 px-5 flex justify-between items-center">
+              <a href="/"><span className="text-xl lg:text-2xl">CariMentor</span></a>
+              <button id="mobile-navbar-btn" onClick={toggleMobileNavBar} className="bg-darkgreen p-3 rounded-full md:hidden"><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></button>
+            </div>
+            <div className="hidden md:block">
+              <a className="mx-5" href="https://docs.google.com/forms/d/e/1FAIpQLSfuHXLW-LRdI4f7g0UeDa2nAmL9kSZ8gaTHKSqpMNwXLM-vRg/viewform?usp=sf_link" >Be A Mentor</a>
+              <a className="mx-5" href="https://docs.google.com/forms/d/e/1FAIpQLSeK0D3-JwopdRAAwLJK6QbonMAsSBEkafhIyf8XMUfu78eNxg/viewform?usp=sf_link" >Be A Mentee</a>
+              <a className="mx-5" href="/about" >About</a>
+            </div>
+            <div className="hidden md:block">
+              <a href="#sign-up-now" className="text-white bg-darkgreen p-2 rounded-2xl ">Get Start</a>
+            </div>
           </div>
             <ul className="px-7 py-4 hidden" id="mobile-navbar">
             <li className="pb-2 underline"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfuHXLW-LRdI4f7g0UeDa2nAmL9kSZ8gaTHKSqpMNwXLM-vRg/viewform?usp=sf_link" >Be A Mentor</a></li>
@@ -24,6 +34,7 @@ const NavBar = () => {
             <br />
             <a href="#sign-up-now" className="text-white bg-darkgreen p-2 rounded-2xl ">Get Start</a>
             </ul>
+
         </nav>
     </>
   )
